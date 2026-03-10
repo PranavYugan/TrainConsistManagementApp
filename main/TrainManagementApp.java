5898e4a0351ef278563ac0a82f2f730d3332d9b3
PranavYugan <yuganpranav@gmail.com>
2026-03-10 14:08:06 +0530

    Add passenger bogies to train

--- main/TrainManagementApp.java
+++ main/TrainManagementApp.java
@@ -4,33 +4,55 @@ import java.util.*;
 /**
  * MAIN CLASS
  * 
- * Use Case 1 : Initialize Train and Display Consist Summary
+ * Use Case 2 : Add Passenger Bogies to train
  * 
  * Description:
- * This class represents the entry point of the Train Consist 
- * Management Application.
+ * This class demonstrates how passenger bogies can be 
+ * managed dynamically using ArrayList operations. 
  * 
- * This use case introduces collection initialization and 
- * basic program startup flow.
+ * This use case maps CRUD operations using ArrayList.
  * 
  * @author Developer
- * @version 1.0
+ * @version 2.0
  * 
  */
 public class TrainManagementApp {
 
 	public static void main(String[] args) {
-		System.out.println("================================");
-		System.out.println("==Train Consist Management App==");
-		System.out.println("================================");
+		System.out.println("=========================================");
+		System.out.println("==UC 2 : Add Passengers to Bogies Train==");
+		System.out.println("=========================================");
 		
 		List<String> trainConsist = new ArrayList<>();
 		System.out.println("Train Initialized Successfully.");
-		
-		System.out.println("Initial Bogie Count : " + trainConsist.size());
-		System.out.println("Current Train Consist : " + trainConsist);
 		System.out.println();
-		System.out.println("System ready for operations...");
+		System.out.println("Before adding Bogies : " );
+		System.out.println(trainConsist);
+		
+		trainConsist.add("Sleeper");
+		trainConsist.add("AC Chair");
+		trainConsist.add("First Class");
+		System.out.println();
+		System.out.println("After adding Bogies : " );
+		System.out.println(trainConsist);
+		System.out.println();
+		trainConsist.remove(1);
+		System.out.println();
+		System.out.println("After removing 'AC Chair' : " );
+		System.out.println(trainConsist);
+		System.out.println();
+		System.out.println("Checking if Sleeper exists : " );
+		System.out.println("Contains Sleeper ? : " + trainConsist.contains("Sleeper"));
+		System.out.println();
+		System.out.println("Final Train Passenger Consist : " );
+		System.out.println(trainConsist);
+		
+		System.out.println();
+		
+		System.out.println("UC2 Operations completed successfully");
+		
+		
+		
 
 		
 	}
